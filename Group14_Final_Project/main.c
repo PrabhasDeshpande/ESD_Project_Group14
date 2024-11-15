f0619e36985d6f5f99d75beceb1c6f8f2fe81193
Prabhas <ee24ms002@iitdh.ac.in>
2024-11-15 19:45:02 +0530

    added test audio data; add UART next.. last

--- Group14_Final_Project/main.c
+++ Group14_Final_Project/main.c
@@ -4,32 +4,39 @@
 #include <tm4c123gh6pm.h>
 #define CLOCK  16000000
 #define DAC_I2C_ADDRESS 0x58
+#define L 6
 
 
 void GPIO_PORTF_INIT(void);
 
-void Systick_Handler(void);
+void Systick_Handler(void);     // send data on interrupt to DAC: 23us
 void systick(float time);
 
+uint8_t audio[L] = {0x10, 0x20, 0x7F, 0xAB, 0xC9, 0xFF}; // add data here.....
 
-void I2C0_Init(void);
-void DFR0971_Write(uint8_t command, uint8_t data);
+int i = 0;     // index counter
+
+
+void I2C1_Init(void);
+void set_slave_addr(void);
+void DFR0971_Write(uint8_t data , uint8_t command);
 
 
 void main(void)
 {
 
-    SYSCTL_RCGCGPIO_R |= 0x21;  //enable clock to portA and portF
+    I2C1_Init();
+    set_slave_addr();
 
-    GPIO_PORTF_INIT();
-    //systick(22);
-
-    I2C0_Init();
-    DFR0971_Write(01,11);
+    DFR0971_Write(0x02,0x03); // DAC setting and I2C: start-run
 
     while(1)
     {
+         systick(46);   // output at 22KHz
+         i++;
 
+         if(i == L-1)
+             i = 0;
 
     }
 
@@ -37,34 +44,39 @@ void main(void)
 
 
 // Function to initialize I2C
-void I2C0_Init(void) {
-    SYSCTL_RCGCI2C_R |= 0x01;            // Enable clock to I2C0
-    SYSCTL_RCGCGPIO_R |= 0x02;           // Enable clock to Port B
+void I2C1_Init(void) {
+    SYSCTL_RCGCI2C_R = (1 << 1);            // Enable clock to I2C1
+    SYSCTL_RCGCGPIO_R = (1 << 1);           // Enable clock to Port B
     while ((SYSCTL_PRGPIO_R & 0x02) == 0); // Wait until GPIOB is ready
 
-    GPIO_PORTB_AFSEL_R |= 0x0C;          // Enable alternate function on PB2, PB3
-    GPIO_PORTB_ODR_R |= 0x08;            // Enable open-drain on PB3 (SDA)
-    GPIO_PORTB_DEN_R |= 0x0C;            // Enable digital I/O on PB2, PB3
-    GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & 0xFFFF00FF) | 0x00003300; // Set I2C function on PB2, PB3
+    GPIO_PORTB_AFSEL_R = (1 << 6) | (1 << 7);          // Enable alternate function on PB2, PB3
 
-    I2C0_MCR_R = 0x10;                   // Enable I2C0 Master function
-    I2C0_MTPR_R = 24;                    // Set SCL clock speed to 100 kHz with 16 MHz system clock
+
+
+    GPIO_PORTB_DEN_R = (1 << 6) | (1 << 7);       // Enable digital I/O on PB2, PB3
+    GPIO_PORTB_ODR_R  = (1 << 7)   ;        // Enable open-drain on PB3 (SDA)       // Enable open-drain on PB3 (SDA)
+    GPIO_PORTB_PCTL_R = (3 << 28) | (3 << 24);      // Set I2C function on PB2, PB3
+
+    I2C1_MCR_R = (1 << 4);                   // Enable I2C1 Master function
+    I2C1_MTPR_R = (7 << 0);                    // Set SCL clock speed to 100 kHz with 16 MHz system clock
 }
 
+void set_slave_addr(void)
+{
+    I2C1_MSA_R = DAC_I2C_ADDRESS << 1 ;
+    I2C1_MSA_R &= !(1 << 0);
+
+}
 
 // Function to send data to a specific channel on DFR0971
-void DFR0971_Write(uint8_t command, uint8_t data) {
-    I2C0_MSA_R = (DAC_I2C_ADDRESS << 1); // Set the I2C address with write bit
-    I2C0_MDR_R = command;                // Write command for specific channel
-    I2C0_MCS_R = 0x03;                   // Start and Run condition
-    while (I2C0_MCS_R & 0x01);           // Wait for I2C transmission
+void DFR0971_Write(uint8_t data, uint8_t commands) {
 
-    I2C0_MDR_R = data;                   // Write the data (audio sample)
-    I2C0_MCS_R = 0x05;                   // Run and Stop condition
-    while (I2C0_MCS_R & 0x01);           // Wait for transmission to complete
+    I2C1_MDR_R = data;
+    I2C1_MCS_R = commands;
+    while (I2C1_MCS_R & 0x01);           // Wait for I2C transmission
 
-    if (I2C0_MCS_R & 0x02) {             // Check for errors
-        I2C0_MCS_R = 0x04;               // Send stop condition if error
+    if (I2C1_MCS_R & 0x02) {             // Check for errors
+        I2C1_MCS_R = 0x04;               // Send stop condition if error
     }
 }
 
@@ -88,7 +100,19 @@ void systick(float time)
 
 void Systick_Handler(void)
 {
-    GPIO_PORTF_DATA_R ^= (0x04);
+
+    if(i < L-1)
+    {
+        DFR0971_Write(audio[i] - 0x80,0x01);    // unsigned 8 bit wav file has 0x80 as zero // run-run-run
+
+    }
+
+    else if(i == L-1)
+    {
+
+        DFR0971_Write(audio[i] - 0x80,0x05);    // run-stop
+
+    }
 
 }
 
